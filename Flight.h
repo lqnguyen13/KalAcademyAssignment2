03dd14ca8e74821ea6dbb05f22788784473e133e
arpitaojha1983 <abarpita@yahoo.com>
2018-07-17 00:33:01 -0700

    Added flight.h and flight.cpp

--- Flight.h
+++ Flight.h
@@ -12,9 +12,42 @@ namespace AirlineReservationSystem {
 		std::string mArriveTime;
 		float mFlightDur;
 		int mTotalSeats;
-		int mSeatsReserved;
+		bool mReserved = false;
+	
+public:
+	// TODO: WRITE SETTERS/GETTERS AND METHODS IN HEADER FILE
+	
+	Flight() = default;
+	Flight(const std::string& depart,
+		const std::string& arrive,
+		int flightno);
 
-	public:
-		// TODO: WRITE SETTERS/GETTERS AND METHODS IN HEADER FILE
+	void displayFlight() const;
+
+	//Getters and setters
+
+
+	bool isReserved() const;
+
+	void setFlightno(int flightno);
+	int getFlightno() const;
+
+	void setDepartLoc(const std::string& depart);
+	const std::string& getDepartLoc() const;
+
+	void setArriveLoc(const std::string& arrive);
+	const std::string& getArriveLoc() const;
+
+	void setDepartTime(const std::string& departtime);
+	const std::string& getDepartTime() const;
+
+	void setArriveTime(const std::string& arrivetime);
+	const std::string& getArriveTime() const;
+
+	void setFlightDuration(float duration);
+	float getFlightDuration() const;
+
+	void setTotalSeat(int noofseat);
+	int getTotalSeat() const;
 	};
 }
